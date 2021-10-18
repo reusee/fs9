eba17da685410ed2e1c5a04145e316e9c5e202f7
reus <reusee@gmail.com>
2021-10-18 11:15:24 +0800

    refine testFS

--- fs_test.go
+++ fs_test.go
@@ -4,6 +4,9 @@ import (
 	"bytes"
 	"fmt"
 	"io"
+	"math/rand"
+	pathpkg "path"
+	"strings"
 	"testing"
 
 	"github.com/reusee/e4"
@@ -15,25 +18,50 @@ func testFS(
 ) {
 	defer he(nil, e4.TestingFatal(t))
 
-	// make dir
-	err := fs.MakeDir("foo")
-	ce(err)
+	dirNames := []string{"foo", "bar", "baz", "qux", "quux"}
+	randomPath := func() string {
+		slice := make([]string, rand.Intn(len(dirNames)))
+		for i := range slice {
+			slice[i] = dirNames[rand.Intn(len(dirNames))]
+		}
+		slice = append(slice, fmt.Sprintf("%d", rand.Int63()))
+		return strings.Join(slice, "/")
+	}
 
-	// write
-	h, err := fs.OpenHandle("foo/bar", OptCreate(true))
-	ce(err)
-	_, err = fmt.Fprintf(h, "foo")
-	ce(err)
-	ce(h.Close())
+	for i := 0; i < 1024; i++ {
+		path := randomPath()
+		pt("%s\n", path)
 
-	// read
-	f, err := fs.Open("foo/bar")
-	ce(err)
-	content, err := io.ReadAll(f)
-	ce(err)
-	ce(f.Close())
-	if !bytes.Equal(content, []byte("foo")) {
-		t.Fatal()
+		// make dir
+		err := fs.MakeDirAll(pathpkg.Dir(path))
+		ce(err)
+
+		// write
+		h, err := fs.OpenHandle(path, OptCreate(true))
+		ce(err)
+		_, err = fmt.Fprintf(h, "foo")
+		ce(err)
+		ce(h.Close())
+
+		// info
+		f, err := fs.Open(path)
+		ce(err)
+		info, err := f.Stat()
+		ce(err)
+		if info.Name() != pathpkg.Base(path) {
+			t.Fatal()
+		}
+		if info.Size() != 3 {
+			t.Fatalf("got %d", info.Size())
+		}
+
+		content, err := io.ReadAll(f)
+		ce(err)
+		ce(f.Close())
+		if !bytes.Equal(content, []byte("foo")) {
+			t.Fatalf("got %s", content)
+		}
+
 	}
 
 	// fstest TODO
