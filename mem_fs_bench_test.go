c7402bd75d2a835cac8815669c024daae8cd5aa9
reus <reusee@gmail.com>
2021-11-07 01:27:26 +0800

    call SetBytes in BenchmarkMemFSWrite

--- mem_fs_bench_test.go
+++ mem_fs_bench_test.go
@@ -1,17 +1,22 @@
 package fs9
 
-import "testing"
+import (
+	"bytes"
+	"testing"
+)
 
 func BenchmarkMemFSWrite(b *testing.B) {
 	fs := NewMemFS()
 	f, err := fs.OpenHandle("foo", OptCreate(true))
 	ce(err)
 	defer f.Close()
+	bs := bytes.Repeat([]byte("a"), 4096)
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		_, err := f.Seek(0, 0)
 		ce(err)
-		_, err = f.Write([]byte("foo"))
+		_, err = f.Write(bs)
 		ce(err)
+		b.SetBytes(int64(len(bs)))
 	}
 }
