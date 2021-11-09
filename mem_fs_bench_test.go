9e08f922b0ba7464342b36e4be7baafd8478c96b
reus <reusee@gmail.com>
2021-11-10 01:00:44 +0800

    add parallel write benchmark

--- mem_fs_bench_test.go
+++ mem_fs_bench_test.go
@@ -20,3 +20,21 @@ func BenchmarkMemFSWrite(b *testing.B) {
 		b.SetBytes(int64(len(bs)))
 	}
 }
+
+func BenchmarkMemFSParallelWrite(b *testing.B) {
+	fs := NewMemFS()
+	bs := bytes.Repeat([]byte("a"), 4096)
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		f, err := fs.OpenHandle("foo", OptCreate(true))
+		ce(err)
+		defer f.Close()
+		for pb.Next() {
+			_, err := f.Seek(0, 0)
+			ce(err)
+			_, err = f.Write(bs)
+			ce(err)
+			b.SetBytes(int64(len(bs)))
+		}
+	})
+}
